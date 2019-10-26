356d793796f5738be205b649a4e3c7448cecaf4d
Chris Conlon <Chris.Conlon1993@gmail.com>
2019-10-26 11:58:52 -0400

    Removed unused imports and objects

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -7,8 +7,6 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { PiBurndownComponent } from './tiles/pi-burndown/pi-burndown.component';
 
@@ -17,8 +15,6 @@ import { PiBurndownComponent } from './tiles/pi-burndown/pi-burndown.component';
     AppComponent,
     NavMenuComponent,
     HomeComponent,
-    CounterComponent,
-    FetchDataComponent,
     SidebarComponent,
     PiBurndownComponent
   ],
@@ -28,8 +24,6 @@ import { PiBurndownComponent } from './tiles/pi-burndown/pi-burndown.component';
     FormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
       { path: 'pi-burndown', component: PiBurndownComponent},
     ])
   ],
