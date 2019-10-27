994de82ae9f214d0d99b31ce7a497d5dd969efda
Chris Conlon <Chris.Conlon1993@gmail.com>
2019-10-26 20:45:58 -0400

    Added line chart

--- ClientApp/src/app/charts/line-chart/line-chart.component.ts
+++ ClientApp/src/app/charts/line-chart/line-chart.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+const sampleData: any[] = [
+  {data: [65,70,75,80,100], label: "Pi"},
+  {data: [25,50,30,35,40], label: "Burndown"}
+];
+
+const sampleLabels: string[] = ["Sprint 1", "Sprint 2", "Sprint 3", "Sprint 4", "Sprint 6"];
+
 @Component({
   selector: 'app-line-chart',
   templateUrl: './line-chart.component.html',
@@ -8,6 +15,15 @@ import { Component, OnInit } from '@angular/core';
 export class LineChartComponent implements OnInit {
 
   constructor() { }
+
+  public lineChartData: any[] = sampleData;
+  public lineChartLabels: string[] = sampleLabels;
+  public lineChartOptions: any={
+    scaleShowVerticaleLines: false,
+    responsive: true
+  };
+  public lineChartLegend = true;
+  public lineChartType = 'line';
 
   ngOnInit() {
   }
