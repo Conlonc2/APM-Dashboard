293498a2ccd5409ca0c37cb5f94c09eb88e2f775
Chris Conlon <Chris.Conlon1993@gmail.com>
2019-11-24 12:07:28 -0500

    Added part of the velocity chart

--- ClientApp/src/app/charts/velocity/velocity.component.ts
+++ ClientApp/src/app/charts/velocity/velocity.component.ts
@@ -1,5 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+const sampleData: any[] = [
+  {data: [85,76,69,70,69], label: "Actual", type: "line", fill: false},
+  {data: [65,75,74,71,69], label: "Predicted"}
+];
+
+const sampleLabels: string[] = [
+  "S1", "S2", "S3", "S4", "S5"
+]
+
 @Component({
   selector: 'app-velocity',
   templateUrl: './velocity.component.html',
@@ -8,7 +17,14 @@ import { Component, OnInit } from '@angular/core';
 export class VelocityComponent implements OnInit {
 
   constructor() { }
-
+  public chartData: any[] = sampleData;
+  public chartLabels: any[] = sampleLabels;
+  public chartOptions: any = {
+    scaleShowVerticaleLines: false,
+    responsive: true
+  };
+  public chartLegend = true;
+  public chartType = "bar"
   ngOnInit() {
   }
 
