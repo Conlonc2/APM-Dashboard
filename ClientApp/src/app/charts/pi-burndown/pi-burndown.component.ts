903ba8b9c940e6a932a4fb0d2e76cb7d96a3ae64
Chris Conlon <Chris.Conlon1993@gmail.com>
2019-12-11 11:25:02 -0500

    Velocity now displayed and synced to DB

--- ClientApp/src/app/charts/pi-burndown/pi-burndown.component.ts
+++ ClientApp/src/app/charts/pi-burndown/pi-burndown.component.ts
@@ -3,7 +3,7 @@ import { DbGrabComponent } from 'src/app/db-grab/db-grab.component';
 
 const sampleData: any[] = [
   { data: [65, 84, 72, 61, 65, 65, 84, 72, 61, 65], label: "Actual", fill: false, type: "line" },
-  { data: [85, 67, 55, 70, 67, 65, 84, 72, 61, 65], label: "Effort", type: "bar" }
+  { data: [100, 67, 55, 70, 67, 65, 84, 72, 61, 65], label: "Effort", type: "bar" }
 ];
 
 const sampleLabels: string[] = ["S1", "S2", "S3", "S4", "S5"];
@@ -26,7 +26,6 @@ export class PiBurndownComponent implements OnInit {
   public lineChartLegend = true;
   public lineChartType = 'bar';
   ngOnInit() {
-    let i = 0;
     this._db.gatherData()
       .subscribe((res) => {
         for (let index in res) {
--- ClientApp/src/app/charts/velocity/velocity.component.ts
+++ ClientApp/src/app/charts/velocity/velocity.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { DbGrabComponent } from 'src/app/db-grab/db-grab.component';
 
 const sampleData: any[] = [
-  {data: [85,76,69,70,69,65,75,74,71,69], label: "Actual", type: "line", fill: false},
-  {data: [65,75,74,71,69,85,76,69,70,69], label: "Predicted"}
+  { data: [85, 76, 69, 70, 69, 65, 75, 74, 71, 69], label: "Actual", type: "line", fill: false },
+  { data: [65, 75, 74, 71, 69, 85, 76, 69, 70, 69], label: "Predicted" }
 ];
 
-const sampleLabels: string[] = ["S1", "S2", "S3", "S4", "S5", "S6", "S7", "S8", "S9", "S10"];
+const sampleLabels: string[] = ["S1", "S2", "S3", "S4", "S5"];
 
 @Component({
   selector: 'app-velocity',
@@ -13,8 +14,9 @@ const sampleLabels: string[] = ["S1", "S2", "S3", "S4", "S5", "S6", "S7", "S8", 
   styleUrls: ['./velocity.component.css']
 })
 export class VelocityComponent implements OnInit {
+  data:any[] = [];
 
-  constructor() { }
+  constructor(private _db: DbGrabComponent) { }
   public chartData: any[] = sampleData;
   public chartLabels: any[] = sampleLabels;
   public chartOptions: any = {
@@ -24,6 +26,16 @@ export class VelocityComponent implements OnInit {
   public chartLegend = true;
   public chartType = "bar"
   ngOnInit() {
+    this._db.gatherData().subscribe((res) => {
+      for (let index in res) {
+        this.data.push(res[index])
+      }
+      this.chartData = [
+        {data:[this.data[0]["velocity"],this.data[1]["velocity"],this.data[2]["velocity"],this.data[3]["velocity"],this.data[4]["velocity"],this.data[5]["velocity"]],label: "Actual", type: "line", fill: false  },
+        {data:[this.data[0]["velocity_predicted"],this.data[1]["velocity_predicted"],this.data[2]["velocity_predicted"],this.data[3]["velocity_predicted"],this.data[4]["velocity_predicted"],this.data[5]["velocity_predicted"]],label: "Predicted"}
+        
+      ]
+    })
   }
 
 }
